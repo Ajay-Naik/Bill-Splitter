61ec02607fd5c2e3d92a000ed4a1ef5c39026335
ajay naik <ajaynaik6853@gmail.com>
2025-09-14 15:56:42 +0530

    deployed

--- Bill-Splitter/src/main.jsx
+++ Bill-Splitter/src/main.jsx
@@ -7,7 +7,7 @@ import Manual from "./pages/Manual.jsx";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/Bill-Splitter">
+  <BrowserRouter basename="/App.jsx">
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/scanner" element={<Scanner />} />
