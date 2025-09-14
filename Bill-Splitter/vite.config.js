1289e0eb223bbfd4706b526343cb084bbcb83218
Ajay Naik <139127037+Ajay-Naik@users.noreply.github.com>
2025-09-14 17:09:13 +0530

    changes

--- Bill-Splitter/vite.config.js
+++ Bill-Splitter/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue'; 
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/Bill-Splitter/"
-})
+  base: '/Bill-Splitter/',
+  plugins: [vue()],
+});
